66f507630a3866d00475fc9f6d2683a951adf527
Paul <paulmichaelhardman@gmail.com>
2022-11-08 11:10:29 +0000

    .

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-import {StyleSheet, Text, View, PermissionsAndroid} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  PermissionsAndroid,
+  Platform,
+} from 'react-native';
 import React, {useState} from 'react';
 import Map from './components/Map';
 
 const App = () => {
   const [hasMapPermissions, setHasMapPermissions] = useState(true);
+
+  
 
   return <View>{hasMapPermissions && <Map />}</View>;
 };
